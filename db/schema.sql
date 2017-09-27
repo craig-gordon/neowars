935899a94fb1cf5e0be6dab34e77944849cdbb9c
Craig Gordon <cyghfer@gmail.com>
2017-09-27 11:59:32 -0400

    Add maps and spaces tables to Schema

--- db/schema.sql
+++ db/schema.sql
@@ -6,13 +6,16 @@ USE neowars;
 
 CREATE TABLE games (
   id int NOT NULL AUTO_INCREMENT,
+  map varchar(20) NOT NULL,
   current_day int NOT NULL,
   current_turn varchar(20) NOT NULL,
   country_one varchar(20) NOT NULL,
   country_two varchar(20) NOT NULL,
   country_three varchar(20),
   country_four varchar(20),
-  PRIMARY KEY (id)
+  PRIMARY KEY (id),
+  FOREIGN KEY (map)
+  REFERENCES maps(id)
 );
 
 CREATE TABLE units (
@@ -24,6 +27,26 @@ CREATE TABLE units (
   PRIMARY KEY (id),
   FOREIGN KEY (game_id)
   REFERENCES games(id)
+);
+
+CREATE TABLE maps (
+  id int AUTO_INCREMENT,
+  name varchar(20) NOT NULL,
+  height int NOT NULL,
+  width int NOT NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE spaces (
+  id int AUTO_INCREMENT,
+  map_id int,
+  row_no int NOT NULL,
+  col_no int NOT NULL,
+  type varchar(10) NOT NULL,
+  capture_points int,
+  PRIMARY KEY (id),
+  FOREIGN KEY (map_id)
+  REFERENCES maps(id)
 );
 
 /*  Execute this file from the command line by typing:
