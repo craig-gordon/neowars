ebcc7508889edb7062dc0bdf35f83c9bd3d08067
Craig Gordon <cyghfer@gmail.com>
2017-11-16 15:02:10 -0500

    Attempt to debug server problem

--- db/index.js
+++ db/index.js
@@ -4,16 +4,17 @@ const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, proces
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
   dialect: 'mysql',
-
   define: {
     freezeTableName: true
   },
-
+  dialectOptions: {
+      ssl:'Amazon RDS'
+  },
   pool: {
-    max: 5,
-    min: 0,
-    idle: 10000
-  }
+    maxConnections: 5,
+    maxIdleTime: 30
+  },
+  language: 'en'
 });
 
 sequelize.authenticate()
